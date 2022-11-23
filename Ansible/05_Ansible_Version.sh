0b88d288ec7bb3838eed60e98c9b2a641a5e575f
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-23 15:59:29 +0100

    lines edited

--- Ansible/05_Ansible_Version.sh
+++ Ansible/05_Ansible_Version.sh
@@ -2,7 +2,7 @@ ansible --version
 
 
 
-As of MAy 2021, the steps taken will install Ansible 2.9.xx no matter which distribution was
+As of May 2021, the steps taken will install Ansible 2.9.xx no matter which distribution was
 chosen as the controller node.
 
 
