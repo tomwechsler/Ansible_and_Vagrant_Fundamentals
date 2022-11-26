ff65ebde4df76baf75199a2491f92f6f8e43367a
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-26 17:52:27 +0100

    new file

---
+++ Ansible/A06/06_02_Handlers.sh
@@ -0,0 +1,28 @@
+#Handlers
+#Handlers can be used to start an action after a task. However, if no change is made with the task, the handler is not called. 
+#Let's look at this with an example.
+
+vim handlers.yaml
+
+---
+
+- hosts: server
+  become: yes
+  tasks:
+   - name: install vsftpd on ubuntu
+     apt: name=vsftpd update_cache=yes state=latest
+	 ignore_errors: yes
+	 notify: start vsftpd
+   
+   - name: install vsftpd on centos
+     yum: name=vsftpd state=latest
+	 ignore_errors: yes
+	 notify: start vsftpd
+
+  handlers:
+   - name: start vsftpd
+     service: name=vsftpd enabled=yes state=started
+
+
+#Make note that the handler has been executed. When the playbook is started again, 
+#the handler will not be called again because there was no change.
