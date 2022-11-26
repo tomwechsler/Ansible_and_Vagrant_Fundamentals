dc75096aef5a792d206514448e73e412ee518539
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-26 18:00:12 +0100

    new file

---
+++ Ansible/A06/06_03_Variables_and_facts.sh
@@ -0,0 +1,33 @@
+#Variables and facts
+
+vim facts.yaml
+
+---
+
+- hosts: centos
+  vars:
+  - var1: thats great
+  - var2: is even better
+  
+  tasks:
+   - name: echo stuff
+     shell: echo " {{ var1 }} is var1, but var2 {{ var2 }}" > /home/vagrant/facts.txt
+
+
+#Now we have worked with the first variables. Now imagine if we can create the variables based on facts. 
+#The following example:
+
+ansible centos -m setup
+
+ansible centos -m setup -a "filter=*family*"
+
+#and now the following adjustment in the playbook
+
+- hosts: centos
+  vars:
+  - var1: thats great
+  - var2: is even better
+  
+  tasks:
+   - name: echo stuff
+     shell: echo " {{ var1 }} var1, but var2 {{ var2 }}" > /home/vagrant/{ ansible_os_failiy }}.txt
