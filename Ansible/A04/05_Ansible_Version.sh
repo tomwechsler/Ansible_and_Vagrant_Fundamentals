f4eda8593e0cf74f35e3dd0e8809098e2ff62c7d
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-26 16:30:22 +0100

    new lines

--- Ansible/A04/05_Ansible_Version.sh
+++ Ansible/A04/05_Ansible_Version.sh
@@ -5,7 +5,8 @@ ansible --version
 As of May 2021, the steps taken will install Ansible 2.9.xx no matter which distribution was
 chosen as the controller node.
 
-
+#Generate a sample ansible.cfg file
+ansible-config init --disabled > ansible.cfg
 
 
 
