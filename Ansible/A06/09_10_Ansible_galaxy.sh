969cce787a68384bcacf9056e5c74bf99a388a39
Tom Wechsler <tom.wechsler@outlook.com>
2022-11-28 07:15:23 +0100

    new files and lines edited

--- Ansible/A06/09_10_Ansible_galaxy.sh
+++ Ansible/A06/09_10_Ansible_galaxy.sh
@@ -1,2 +1,26 @@
 #Ansible Galaxy
 
+
+https://galaxy.ansible.com/
+
+#The templates which are downloaded from Galaxy are in the directory:
+
+ls /etc/ansible/roles
+
+ansible-galaxy search freeipa
+
+#Attention when choosing, make sure that the template supports your distribution.
+
+sudo ansible-galaxy install yabhinav.ipaserver
+
+ls /etc/ansible/roles
+
+#vim ipa.yaml
+
+---
+
+- name: ipa server
+- hosts: server
+  become: yes
+  roles:
+   - yabhinav.ipaserver
