d20c54fdb232eac335085b8f725fe486571b5a40
Tom Wechsler <tom.wechsler@outlook.com>
2023-05-15 14:54:09 +0200

    lines edited

--- Vagrant/A02/Vagrant_Commands.sh
+++ Vagrant/A02/Vagrant_Commands.sh
@@ -66,11 +66,13 @@ Vagrant.configure("2") do |config|
 	config.vm.define "test1" do |test1|
 		test1.vm.hostname = "test1"
 		test1.vm.network "private_network", ip: "192.168.56.101"
+		test1.vm.disk :disk, size: "30GB", primary: true
 	end
 
 	config.vm.define "test2" do |test2|
 		test2.vm.hostname = "test2"
 		test2.vm.network "private_network", ip: "192.168.56.102"
+		test2.vm.disk :disk, size: "30GB", primary: true
 	end
 end
 
